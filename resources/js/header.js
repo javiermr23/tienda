03ed48792017c51f207b600b6b89de8f9d08fd68
Sergio Gutiérrez <sergio_gutierrez_1@hotmail.com>
2019-02-07 09:16:31 +0100

    Despliegue del menú que cumpla con los estándares del jefe

--- resources/js/header.js
+++ resources/js/header.js
@@ -1,9 +1,13 @@
 
-$(document).ready(function () {
-    
-    $("header .menu>img").click(function(){
-        $("header nav").slideToggle();
-        console.log("ojete")
-    });
+let elemHamburguesa = document.querySelector("header div.menu>img");
+let elemNav = document.querySelector("header nav");
 
+//Evento para desplegar el menú
+elemHamburguesa.addEventListener('click', ()=>{
+    elemNav.classList.toggle('visible');
 });
+
+//Desplegar el menú al cargar la página
+setTimeout(() => {
+    elemNav.classList.toggle('visible');
+}, 500);

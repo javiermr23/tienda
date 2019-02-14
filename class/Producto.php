6cebaa6d510418d8e5b73ab2fdd44073760bcfdc
javiermr23 <javiermr@alumnos.iesgalileo.es>
2019-02-14 08:28:35 +0100

    Cambios en la clase 'Producto'

--- class/Producto.php
+++ class/Producto.php
@@ -1,17 +1,26 @@
 <?php
     class Producto{
+
+
         public static cargarProducto($id){
             $sql = "SELECT *
-                FROM producto
-                WHERE id=:id";
-            $sentencia = Database::conexion->prepare($sql);
-            $sentencia->bind_param(":id",$id);
+                    FROM producto
+                    WHERE id = :id";
 
-            if($sentencia->execute()){
-                while($fila = $sentencia->fetch()){
-                    print_r($fila);
+            try {
+                $sentencia = Database::$conexion->prepare($sql);
+                $sentencia->bind_param(":id", $id, PDO::PARAM_INT);
+                $sentencia->setFetchMode(PDO::FETCH_ASSOC);
+    
+                if ($sentencia->execute()) {
+                    return $sentencia->fetch();
                 }
             }
+            catch (PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
+            return false;
         }
     }
 ?>

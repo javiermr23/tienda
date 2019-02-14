caf3410710ba4db54dea76a74fac69a5920535a2
Sergio Gutiérrez <sergio_gutierrez_1@hotmail.com>
2019-02-14 08:27:31 +0100

    Sacar cabecera y pie a ficheros externos

--- index.php
+++ index.php
@@ -20,37 +20,10 @@
 </head>
 
 <body>
-    <header>
-        <div class="cabecera">
-            <div class="logo"><img src="resources/img/logos/logo.png" alt="Logo"></div>
-            <div class="menu"><img src="resources/img/iconos/menu.svg" alt="Menú"></div>
-            <div class="buscar">
-                <div>
-                    <img src="resources/img/iconos/search.svg" alt="Buscar">
-                </div>
-                <input type="text" name="buscar" placeholder="Busca un producto.">
-            </div>
-            <div class="usuario">
-                <div><img src="resources/img/iconos/login.svg" alt="Login"></div>
-                <div><img src="resources/img/iconos/user.svg" alt="Cuenta"></div>
-                <div>
-                    <img id="carrito" src="resources/img/iconos/cart.svg" alt="Carrito">
-                    <div id="cesta"></div>
-                </div>
-            </div>
-        </div>
-        <nav>
-            <a href=""><img src="resources/img/iconos/header/tarjetas-graficas.svg" alt="Gráficas">Tarjetas Gráficas</a>
-            <a href=""><img src="resources/img/iconos/header/procesadores.svg" alt="Procesadores">Procesadores</a>
-            <a href=""><img src="resources/img/iconos/header/memoria-ram.svg" alt="RAM">Memoria RAM</a>
-            <a href=""><img src="resources/img/iconos/header/fuentes-alimentacion.svg" alt="Fuentes">Fuentes de
-                alimentación</a>
-            <a href=""><img src="resources/img/iconos/header/placas-base.svg" alt="Placas">Placas base</a>
-            <a href=""><img src="resources/img/iconos/header/discos-duros.svg" alt="HDD">Discos Duros</a>
-            <a href=""><img src="resources/img/iconos/header/cajas.svg" alt="Cajas">Cajas</a>
-            <a href=""><img src="resources/img/iconos/header/ventilacion.svg" alt="Ventilación">Ventilación</a>
-        </nav>
-    </header>
+    <?php
+        require "./html/cabecera.html";
+    ?>
+
     <main>
 
         <div class="slider">
@@ -135,17 +108,10 @@
             </div>
         </div>
     </main>
-    <footer>
-        <p>Práctica conjunta 2ºDAW | Tienda Componentes</p>
-        <div class="rrss">
-            <a href="#"><img src="resources/img/iconos/rrss/facebook.svg" alt="Facebook"></a>
-            <a href="#"><img src="resources/img/iconos/rrss/instagram.svg" alt="Instagram"></a>
-            <a href="#"><img src="resources/img/iconos/rrss/twitter.svg" alt="Twitter"></a>
-            <a href="#"><img src="resources/img/iconos/rrss/google-plus.svg" alt="GooglePlus"></a>
-            <a href="#"><img src="resources/img/iconos/rrss/linkedin.svg" alt="LinkedIn"></a>
-        </div>
-    </footer>
-    <script src="resources/js/header.js"></script>
+
+    <?php
+        require "./html/pie.html";
+    ?>
 </body>
 
 </html>

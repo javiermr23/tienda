e32f34431c42a2bd94ae70fd3547d6574582c713
YerayPS <yerayperezsanjose@gmail.com>
2019-02-14 10:02:01 +0100

    Añadidas funciones de compra  a la clase 'Usuario'

--- class/Usuario.php
+++ class/Usuario.php
@@ -104,6 +104,82 @@
             }
         }
 
+        public static function añadirFavorito($idUsuario,$idProducto){
+            $sql = "INSERT INTO favorito (id_producto, id_usuario) 
+            VALUES (:id_producto, :id_usuario)";
+
+            try {
+                $stmt = Database::$conexion->prepare($sql);
+                Database::$conexion->beginTransaction();
+
+                $stmt->bindValue(":id_producto",$idProducto,PDO::PARAM_INT);
+                $stmt->bindValue(":id_usuario",$idUsuario,PDO::PARAM_INT);
+                $stmt->execute();
+
+                Database::$conexion->commit();
+            } catch (PDOException $e) {
+                Database::$conexion->rollBack();
+                echo $e->getMessage();
+                return false;
+            }
+        }
+
+        // Funciones para realizar compra
+        public static function crearFactura($fecha,$total,$iva, $idUsuario){
+            $sql = "INSERT INTO factura (fecha, total, iva, id_usuario) 
+            VALUES (:fecha, :total, :iva, :id_usuario)";
+
+            try {
+                $stmt = Database::$conexion->prepare($sql);
+                Database::$conexion->beginTransaction();
+
+                $stmt->bindValue(":fecha",$fecha,PDO::PARAM_STR);
+                $stmt->bindValue(":total",$total,PDO::PARAM_STR);
+                $stmt->bindValue(":iva",$iva,PDO::PARAM_INT);
+                $stmt->bindValue(":id_usuario",$idUsuario,PDO::PARAM_INT);
+
+                Database::$conexion->commit();
+            } catch (PDOException $e) {
+                Database::$conexion->rollBack();
+                echo $e->getMessage();
+                return false;
+            }
+        }
+
+        public static function crearLineas($idProducto,$idFactura,$unidades, $importe){
+
+            $sql = "INSERT INTO linea ( id_producto, id_factura, unidades, importe) 
+            VALUES ( :id_producto, :id_factura, :unidades, :importe)";
+
+            try {
+                $stmt = Database::$conexion->prepare($sql);
+                Database::$conexion->beginTransaction();
+
+                $stmt->bindValue(":id_producto",$idProducto,PDO::PARAM_STR);
+                $stmt->bindValue(":id_factura",$idFactura,PDO::PARAM_STR);
+                $stmt->bindValue(":unidades",$unidades,PDO::PARAM_INT);
+                $stmt->bindValue(":importe",$importe,PDO::PARAM_INT);
+
+                Database::$conexion->commit();
+            } catch (PDOException $e) {
+                Database::$conexion->rollBack();
+                echo $e->getMessage();
+                return false;
+            }
+
+        }
+
+        public static function calcularFactura($cesta){
+            $total = 0;
+            foreach ($cesta as $clave => $valor) {
+                $total += $valor['unidades'] * $valor['precio'];
+            }
+            return $total;
+            
+        }
+
+
+
         
 
         

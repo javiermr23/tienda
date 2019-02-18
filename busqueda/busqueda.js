2e065fc1a381e0977ed2657b7a0028cb5ba5f2e9
javiermr23 <javiermr@alumnos.iegalileo.es>
2019-02-18 14:32:36 +0100

    Trabajo avanzado en la interacción con la búsqueda y los filtros

--- busqueda/busqueda.js
+++ busqueda/busqueda.js
@@ -2,22 +2,89 @@
     let ui = (function() {
 
         elms = {
-            hea: {
+            hea: { // Header
                 search: document.querySelector("#cuadroBuscar"),
-
             },
+            bus: { // Busqueda
+                pag: document.querySelector("#pag-busqueda"),
+                fil: document.querySelector(".bus-fil"),
+                clo: document.querySelector(".bus-clo")
+            }
+        };
+
+        func = {
             bus: {
-                pag: document.querySelector
+                mostrar: function() {
+                    elms.bus.pag.classList.remove("ocultar");
+                    document.querySelector("body").classList.add("fixedHeight");
+                },
+
+                ocultar: function() {
+                    elms.bus.pag.classList.add("ocultar");
+                    document.querySelector("body").classList.remove("fixedHeight");
+                }
+            },
+            pro: {
+                filtrosSeleccionados: function() {
+                    let filtros = {
+                        tipo: new Array(),
+                        marca: new Array()
+                    };
+
+                    Array.from(elms.bus.fil.querySelectorAll(".fil-tipo input[type='checkbox']")).forEach(cur => {
+                        if (cur.checked) {
+                            filtros.tipo.push(cur.id);
+                        }
+                    });
+                    Array.from(elms.bus.fil.querySelectorAll(".fil-marca input[type='checkbox']")).forEach(cur => {
+                        if (cur.checked) {
+                            filtros.marca.push(cur.id);
+                        }
+                    });
+                    return filtros;
+                }
             }
         };
 
         return {
-            elms: elms
+            elms: elms,
+            func: func
         }
 
     })();
 
     let dt = (function() {
+
+        data = {
+            pro: undefined
+        };
+
+        func = {
+            pro: {
+                cargarProductos: function() {
+                    let xhr = new XMLHttpRequest();
+                    xhr.open("GET", "busqueda/ajax.php?function=cargarProductos", true);
+                    xhr.send();
+
+                    xhr.addEventListener("load", function() {
+                        data.pro = JSON.parse(xhr.responseText);
+                    });
+                },
+
+                buscar: function(patron) {
+                    return data.pro.filter(cur => cur["nombre"].toLowerCase().includes(patron.toLowerCase()));
+                },
+
+                filtrar: function(productos, filtros) {
+                    console.log(filtros);
+                }
+            }
+        }
+
+        return {
+            data: data,
+            func: func
+        }
 
     })();
 
@@ -27,16 +94,24 @@
 
             gen: {
                 addEventListeners: function() {
-                    ui.elms.hea.search.addEventListeners("focus", func.bus.mostrar);
-                    ui.elms.hea.search.addEventListeners("blur", func.bus.ocultar);
+                    ui.elms.hea.search.addEventListener("focus", func.bus.mostrar);
+                    ui.elms.bus.clo.addEventListener("click", func.bus.ocultar);
+                    ui.elms.hea.search.addEventListener("input", func.bus.filtrar);
+                    ui.elms.bus.fil.addEventListener("input", func.bus.filtrar);
                 }
             },
             bus: {
-                mostrar: function() {
-
+                filtrar: function(evt) {
+                    let productos = dt.func.pro.buscar(evt.target.value);
+                    dt.func.pro.filtrar(productos, ui.func.pro.filtrosSeleccionados());
                 },
-                ocultar: function() {
 
+                mostrar: function() {
+                    ui.func.bus.mostrar();
+                },
+
+                ocultar: function() {
+                    ui.func.bus.ocultar();
                 }
             }
         };
@@ -48,4 +123,5 @@
     })(ui, dt);
 
     controller.func.gen.addEventListeners();
+    dt.func.pro.cargarProductos();
 }
